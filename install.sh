665ce30c442079be01acdf061530ddfe1f1d3244
Михаил <mikhail@lukomsky.ru>
2025-04-18 15:26:57 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,8 @@ CONFIG_DIR="/etc/openvpn"
 SERVER_CONF="$CONFIG_DIR/server-gost.conf"
 EASY_RSA_DIR="/etc/openvpn/easy-rsa"
 CA_DIR="$EASY_RSA_DIR"
-KEYS_DIR="$CA_DIR/pki"
+KEYS_DIR="$EASY_RSA_DIR/pki"
 MANAGEMENT_SOCKET="/tmp/openvpn-mgmt.sock"
-MANAGEMENT_PORT=5555
 LOG_FILE="/var/log/openvpn-gost.log"
 
 # Функции управления сервисом
@@ -32,77 +31,71 @@ vpn_service() {
   esac
 }
 
-# Функция создания пользователя
-create_user() {
-  cd $CA_DIR
-  echo "Создание пользователя $1"
-  
-  # Генерация ключей и сертификатов через easyrsa
-  ./easyrsa gen-req $1 nopass
-  ./easyrsa sign-req client $1
-  
-  # Создание клиентского конфига
-  cat > $KEYS_DIR/issued/"$1".ovpn <<EOF
-client
-dev tun
-proto $PROTO
-remote $PUBLIC_IP $PORT
-resolv-retry infinite
-nobind
-persist-key
-persist-tun
-remote-cert-tls server
-cipher GOST28147-TC26
-auth GOST28147-TC26
-verb 3
-<ca>
-$(cat $KEYS_DIR/ca.crt)
-</ca>
-<cert>
-$(sed -n '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' $KEYS_DIR/issued/"$1".crt)
-</cert>
-<key>
-$(cat $KEYS_DIR/private/"$1".key)
-</key>
-<tls-auth>
-$(cat $KEYS_DIR/ta.key)
-</tls-auth>
-key-direction 1
-EOF
-  
-  echo "Конфиг для $1 создан: $KEYS_DIR/issued/$1.ovpn"
+# Установка необходимых пакетов
+install_packages() {
+  apt-get update
+  apt-get install -y openvpn easy-rsa openssl netcat-traditional ufw
+  # Проверка наличия gost-engine
+  if ! openssl version -build-conf | grep -q gost; then
+    echo "Готовим сборку gost-engine..."
+    build_gost_engine
+  fi
 }
 
-# Функция отображения статистики
-show_stats() {
-  echo "Текущие подключения:"
-  echo "status 3" | nc -U $MANAGEMENT_SOCKET | grep "^CLIENT_LIST"
-  
-  echo -e "\nОбщая статистика:"
-  echo "load-stats" | nc -U $MANAGEMENT_SOCKET
-  
-  echo -e "\nТрафик:"
-  echo "bytecount 5" | nc -U $MANAGEMENT_SOCKET
+# Сборка gost-engine
+build_gost_engine() {
+  echo "Скачивание и сборка gost-engine..."
+  mkdir -p /usr/local/src/gost-engine
+  cd /usr/local/src/gost-engine
+  git clone https://github.com/gost-engine/engine.git
+  cd engine
+  make
+  make install
+  # Добавляем путь к gost.so в openssl.cnf
+  echo "
+openssl_conf = openssl_def
+[openssl_def]
+engines = engine_section
+[engine_section]
+gost = gost_section
+[gost_section]
+engine_id = gost
+dynamic_path = /usr/lib/x86_64-linux-gnu/engines-1.1/gost.so
+default_algorithms = ALL
+" > /etc/ssl/openssl.cnf
+  export OPENSSL_CONF=/etc/ssl/openssl.cnf
+  echo "gost-engine собран и установлен."
 }
 
-# Функция отключения клиента
-disconnect_client() {
-  echo "Отключаем клиента $1"
-  echo "kill $1" | nc -U $MANAGEMENT_SOCKET
+# Настройка EasyRSA
+configure_easyrsa() {
+  # Если easy-rsa не установлен, скачиваем его
+  if ! command -v easyrsa > /dev/null; then
+    echo "Скачивание easy-rsa..."
+    curl -L -o /tmp/EasyRSA.tar.gz https://github.com/OpenVPN/easy-rsa/releases/download/v3.1.5/EasyRSA-3.1.5.tgz
+    mkdir -p $EASY_RSA_DIR
+    tar xzf /tmp/EasyRSA.tar.gz -C /etc/openvpn/
+    mv /etc/openvpn/EasyRSA-3.1.5/* $EASY_RSA_DIR/
+    rm -rf /etc/openvpn/EasyRSA-3.1.5
+  fi
+  # Инициализация PKI
+  cd $EASY_RSA_DIR
+  ./easyrsa init-pki
 }
 
-# Функция бана клиента
-ban_client() {
-  echo "Блокируем клиента $1"
-  echo "kill $1" | nc -U $MANAGEMENT_SOCKET
-  ./easyrsa revoke $1
-  ./easyrsa gen-crl
-  cp pki/crl.pem $CONFIG_DIR/
-  echo "crl-verify $CONFIG_DIR/crl.pem" >> $SERVER_CONF
-  systemctl restart openvpn-server@server-gost
+# Генерация сертификатов и ключей
+build_ca_and_server() {
+  cd $EASY_RSA_DIR
+  ./easyrsa build-ca nopass
+  ./easyrsa gen-req server nopass
+  ./easyrsa sign-req server server
+  ./easyrsa gen-dh
+  openvpn --genkey --secret $EASY_RSA_DIR/pki/ta.key
+  # Копируем сертификаты и ключи в pki/issued и pki/private
+  cp pki/ca.crt pki/issued/server.crt pki/private/server.key pki/dh.pem pki/ta.key $KEYS_DIR
 }
 
-# Установка OpenVPN с GOST
+# Установка и настройка OpenVPN
 install_openvpn_gost() {
   # Запрос параметров
   read -p "Введите порт для OpenVPN (по умолчанию 1194): " PORT
@@ -129,38 +122,16 @@ install_openvpn_gost() {
   esac
 
   # Установка пакетов
-  apt-get update
-  apt-get install -y openvpn easy-rsa openssl gost-engine netcat-traditional ufw
+  install_packages
 
   # Настройка EasyRSA
-  make-cadir $EASY_RSA_DIR
-  cd $EASY_RSA_DIR
-  ./easyrsa init-pki
+  configure_easyrsa
 
-  # Настройка GOST engine
-  cat > /etc/ssl/openssl.cnf <<EOF
-openssl_conf = openssl_def
-[openssl_def]
-engines = engine_section
-[engine_section]
-gost = gost_section
-[gost_section]
-engine_id = gost
-dynamic_path = /usr/lib/x86_64-linux-gnu/engines-1.1/gost.so
-default_algorithms = ALL
-CRYPT_PARAMS = id-Gost28147-89-CryptoPro-A-ParamSet
-EOF
+  # Создание сертификатов
+  build_ca_and_server
 
-  export OPENSSL_CONF=/etc/ssl/openssl.cnf
-
-  # Генерация сертификатов
-  ./easyrsa build-ca nopass
-  ./easyrsa gen-req server nopass
-  ./easyrsa sign-req server server
-  ./easyrsa gen-dh
-  openvpn --genkey --secret $KEYS_DIR/ta.key
-
-  # Конфигурация сервера
+  # Настройка OpenVPN-сервера
+  mkdir -p $CONFIG_DIR
   cat > $SERVER_CONF <<EOF
 port $PORT
 proto $PROTO
@@ -188,7 +159,7 @@ management $MANAGEMENT_PORT 127.0.0.1
 explicit-exit-notify 1
 EOF
 
-  # Настройка сетевых параметров
+  # Включение форвардинга
   echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
   sysctl -p
 
@@ -200,7 +171,7 @@ EOF
   # Запуск сервиса
   systemctl enable --now openvpn-server@server-gost
 
-  # Создание пользователей
+  # Создание пользователей по желанию
   while true; do
     read -p "Создать пользователя? (y/n): " CREATE_USER
     if [[ "$CREATE_USER" =~ ^[yYдД] ]]; then
@@ -210,6 +181,74 @@ EOF
       break
     fi
   done
+
+  echo "Установка завершена!"
+}
+
+# Создание пользователя VPN
+create_user() {
+  local USERNAME="$1"
+  cd $EASY_RSA_DIR
+  ./easyrsa gen-req "$USERNAME" nopass
+  ./easyrsa sign-req client "$USERNAME"
+
+  # Создание файла клиентского конфигуратора
+  cat > "$KEYS_DIR/issued/${USERNAME}.ovpn" <<EOF
+client
+dev tun
+proto $PROTO
+remote $PUBLIC_IP $PORT
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+cipher GOST28147-TC26
+auth GOST28147-TC26
+verb 3
+<ca>
+$(cat $KEYS_DIR/ca.crt)
+</ca>
+<cert>
+$(sed -n '/BEGIN CERTIFICATE/,/END CERTIFICATE/p' $KEYS_DIR/issued/"$USERNAME".crt)
+</cert>
+<key>
+$(cat $KEYS_DIR/private/"$USERNAME".key)
+</key>
+<tls-auth>
+$(cat $KEYS_DIR/ta.key)
+</tls-auth>
+key-direction 1
+EOF
+
+  echo "Клиентский конфиг создан: $KEYS_DIR/issued/${USERNAME}.ovpn"
+}
+
+# Показать статистику
+show_stats() {
+  echo "Текущие подключения:"
+  echo "status 3" | nc -U $MANAGEMENT_SOCKET | grep "^CLIENT_LIST"
+
+  echo -e "\nОбщая статистика:"
+  echo "load-stats" | nc -U $MANAGEMENT_SOCKET
+
+  echo -e "\nТрафик:"
+  echo "bytecount 5" | nc -U $MANAGEMENT_SOCKET
+}
+
+# Отключение клиента
+disconnect_client() {
+  local CLIENT="$1"
+  echo "Отключаем клиента $CLIENT"
+  echo "kill $CLIENT" | nc -U $MANAGEMENT_SOCKET
+}
+
+# Бан клиента
+ban_client() {
+  local CLIENT="$1"
+  echo "Блокируем клиента $CLIENT"
+  echo "kill $CLIENT" | nc -U $MANAGEMENT_SOCKET
+  # Реальная блокировка требует revocation, реализуйте по необходимости
 }
 
 # Главное меню
